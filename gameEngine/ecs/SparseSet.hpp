a39c83b850ec9fa81690f2c8dffb325e59217f4f
Antton Ducos <antton.ducos@epitech.eu>
2025-09-30 21:00:07 +0200

    refacto: optimization for SparseSet class

--- gameEngine/ecs/SparseSet.hpp
+++ gameEngine/ecs/SparseSet.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <vector>
-#include <stdexcept>
 
 template<typename Entity, typename Component>
 class SparseSet {
@@ -10,18 +9,23 @@ class SparseSet {
             return (e < sparse.size() && sparse[e] != npos);
         }
 
-        Component& get(Entity e) { return data[sparse[e]]; }
+        Component& get(Entity e) { 
+            return data[sparse[e]]; 
+        }
 
-        const Component& get(Entity e) const { return data[sparse[e]]; }
+        const Component& get(Entity e) const { 
+            return data[sparse[e]]; 
+        }
 
-        void insert(Entity e, const Component& c) {
+        template<typename... Args>
+        void emplace(Entity e, Args&&... args) {
             if (e >= sparse.size()) {
                 sparse.resize(e + 1, npos);
             }
             if (!contains(e)) {
                 sparse[e] = dense.size();
                 dense.push_back(e);
-                data.push_back(c);
+                data.emplace_back(std::forward<Args>(args)...);
             }
         }
 
@@ -41,10 +45,32 @@ class SparseSet {
             sparse[e] = npos;
         }
 
+        void reserve(size_t capacity) {
+            dense.reserve(capacity);
+            data.reserve(capacity);
+        }
+
+        void clear() {
+            sparse.clear();
+            dense.clear();
+            data.clear();
+        }
+
+        size_t size() const { 
+            return dense.size(); 
+        }
+
+        bool empty() const { 
+            return dense.empty(); 
+        }
+
         auto begin() { return dense.begin(); }
         auto end()   { return dense.end(); }
         auto begin() const { return dense.begin(); }
         auto end()   const { return dense.end(); }
+
+        std::vector<Component>& components() { return data; }
+        const std::vector<Component>& components() const { return data; }
 
     private:
         static constexpr size_t npos = static_cast<size_t>(-1);
