351b60c9b225f5a12676063f14c409662bbe2e4d
Louka <louka.ortega-cand@epitech.eu>
2025-10-08 13:26:37 +0200

    feat: add component 'Collider'

---
+++ gameEngine/ecs/components/collider/src/Collider.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <bitset>
+#include <variant>
+#include "../../../utils.hpp"
+namespace GameEngine {
+struct HitBox {
+};
+struct Collider {
+    vec2 originTranslation;
+    std::bitset<8> entitySelector;
+    std::variant<vec2, float> size; // vec2 -> box size, float -> radius
+    Collider(vec2 val_translation, std::bitset<8> val_selector, std::variant<vec2, float> val_size) : originTranslation(val_translation), entitySelector(val_selector), size(val_size) {}
+};
+} // namespace GameEngine
