b6199bf3c2fdbfa6cfa22ca37e5797cfe0babf91
Louka <louka.ortega-cand@epitech.eu>
2025-10-08 21:17:17 +0200

    feat: add input management system

--- gameEngine/ecs/components/inputControlled/src/InputControlled.hpp
+++ gameEngine/ecs/components/inputControlled/src/InputControlled.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <string>
+#include <vector>
 namespace GameEngine {
 struct InputControlled {
+    std::vector<int> inputs;
     InputControlled() {}
 };
 } // namespace GameEngine
---
+++ gameEngine/ecs/systems/inputHandler/src/InputHandler.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../../../Registry.hpp"
+#include "../../../System.hpp"
+
+namespace GameEngine {
+    class InputHandler : public System<InputHandler> {
+    public:
+        InputHandler() {
+            requireComponents<GameEngine::InputControlled, GameEngine::Acceleration>();
+        }
+        
+        void onUpdate(Registry& registry, float dt) {
+            updateCount++;
+            registry.each<InputControlled, Acceleration>([dt](auto e, InputControlled& inputs, Acceleration& acceleration) {
+                float accelerationValue = 10.0;
+                for (auto &it : inputs.inputs) {
+                    switch (it) {
+                        case 0:
+                            // registry add entity (shoot)
+                        case 1:
+                            acceleration.y = accelerationValue;
+                        case 2:
+                            acceleration.x = -accelerationValue;
+                        case 3:
+                            acceleration.y = -accelerationValue;
+                        case 4:
+                            acceleration.x = accelerationValue;
+                        default:
+                            break;
+                    }
+                }
+            });
+        }
+        
+        int updateCount = 0;
+    };
+}
