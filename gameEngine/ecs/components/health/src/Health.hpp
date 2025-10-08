848c34feef61f386389f25b175f0f28e4382e284
Louka <louka.ortega-cand@epitech.eu>
2025-10-08 13:27:30 +0200

    feat: add component 'Health'

---
+++ gameEngine/ecs/components/health/src/Health.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace GameEngine {
+struct Health {
+    int currentHp;
+    int maxHp;
+    Health(float val_currentHp = 0, float val_maxHp = 0) : currentHp(val_currentHp), maxHp(val_maxHp) {}
+};
+} // namespace GameEngine
