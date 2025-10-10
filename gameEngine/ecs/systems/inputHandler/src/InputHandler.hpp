21aedd2384eaa7fe99d35a07b0b7f31b810edc28
Louka <louka.ortega-cand@epitech.eu>
2025-10-10 15:24:21 +0200

    feat: add shoot creation

--- gameEngine/ecs/systems/inputHandler/src/InputHandler.hpp
+++ gameEngine/ecs/systems/inputHandler/src/InputHandler.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <cstdint>
 #include "../../../Registry.hpp"
 #include "../../../System.hpp"
+#include "../../../components/health/src/Health.hpp"
+#include "../../../components/damage/src/Damage.hpp"
+#include "../../../components/position/src/Position.hpp"
+#include "../../../components/velocity/src/Velocity.hpp"
+#include "../../../components/acceleration/src/Acceleration.hpp"
 
 namespace GameEngine {
     class InputHandlerSystem : public System<InputHandlerSystem> {
@@ -12,14 +18,20 @@ namespace GameEngine {
         
         void onUpdate(Registry& registry, float dt) {
             updateCount++;
-            registry.each<InputControlled, Acceleration>([dt](auto e, InputControlled& inputs, Acceleration& acceleration) {
+            registry.each<InputControlled, Acceleration>([dt, &registry](auto e, InputControlled& inputs, Acceleration& acceleration) {
                 float accelerationValue = 10.0;
                 acceleration.x = 0;
                 acceleration.y = 0;
                 for (auto &it : inputs.inputs) {
                     switch (it) {
                         case 0:
-                            // registry add entity (shoot)
+                            uint32_t e = registry.create();
+                            registry.emplace<GameEngine::Health>(e, 1, 1);
+                            registry.emplace<GameEngine::Damage>(e, 1);
+                            registry.emplace<GameEngine::Velocity>(e, 1000, 1000);
+                            registry.emplace<GameEngine::Acceleration>(e, 1000);
+                            GameEngine::Position playerPos = registry.get<GameEngine::Position>(e);
+                            registry.emplace<GameEngine::Position>(e, playerPos.x, playerPos.y);
                             break;
                         case 1:
                             acceleration.y = accelerationValue;
