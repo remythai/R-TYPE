b8b58fe3f1510e8d88887db9df65caa760c2167e
remythai <remy.thai@epitech.eu>
2025-10-10 15:03:48 +0200

    test: multiplayer again

--- client/src/core/Core.cpp
+++ client/src/core/Core.cpp
@@ -436,7 +436,9 @@ void CLIENT::Core::graphicsLoop()
                     float x = std::stof(msg.substr(pos1 + 1, pos2 - pos1 - 1));
                     float y = std::stof(msg.substr(pos2 + 1));
                     
-                    if (playerId >= 0 && playerId < 4) {
+                    std::cout << "[SNAPSHOT] Player " << playerId << " at (" << x << ", " << y << ")\n";
+                    
+                    if (playerId >= 0 && playerId < 4 && playerId != _myPlayerId) {
                         players[playerId].position = sf::Vector2f(x, y);
                         players[playerId].sprite.setPosition(x, y);
                     }
