d499a868af16b0bcf3492632e19974866bc2b83c
Louka <louka.ortega-cand@epitech.eu>
2025-10-10 15:36:50 +0200

    fix: remove variable in switch

--- gameEngine/ecs/systems/inputHandler/src/InputHandler.hpp
+++ gameEngine/ecs/systems/inputHandler/src/InputHandler.hpp
@@ -8,6 +8,7 @@
 #include "../../../components/position/src/Position.hpp"
 #include "../../../components/velocity/src/Velocity.hpp"
 #include "../../../components/acceleration/src/Acceleration.hpp"
+#include "../../../components/inputControlled/src/InputControlled.hpp"
 
 namespace GameEngine {
     class InputHandlerSystem : public System<InputHandlerSystem> {
@@ -20,18 +21,20 @@ namespace GameEngine {
             updateCount++;
             registry.each<InputControlled, Acceleration>([dt, &registry](auto e, InputControlled& inputs, Acceleration& acceleration) {
                 float accelerationValue = 1000.0;
+                GameEngine::Position playerPos;
+                uint32_t shoot = -1;
                 acceleration.x = 0;
                 acceleration.y = 0;
                 for (auto &it : inputs.inputs) {
                     switch (it) {
                         case 0:
-                            uint32_t e = registry.create();
-                            registry.emplace<GameEngine::Health>(e, 1, 1);
-                            registry.emplace<GameEngine::Damage>(e, 1);
-                            registry.emplace<GameEngine::Velocity>(e, 1000.0, 1000.0);
-                            registry.emplace<GameEngine::Acceleration>(e, 1000.0);
-                            GameEngine::Position playerPos = registry.get<GameEngine::Position>(e);
-                            registry.emplace<GameEngine::Position>(e, playerPos.x, playerPos.y);
+                            shoot = registry.create();
+                            registry.emplace<GameEngine::Health>(shoot, 1, 1);
+                            registry.emplace<GameEngine::Damage>(shoot, 1);
+                            registry.emplace<GameEngine::Velocity>(shoot, 1000.0, 1000.0);
+                            registry.emplace<GameEngine::Acceleration>(shoot, 1000.0);
+                            playerPos = registry.get<GameEngine::Position>(e);
+                            registry.emplace<GameEngine::Position>(shoot, playerPos.x, playerPos.y);
                             break;
                         case 1:
                             acceleration.y = accelerationValue;
