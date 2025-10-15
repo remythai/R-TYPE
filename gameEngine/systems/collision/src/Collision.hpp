bf8cb9c56e4055687a714e5f467802c38fe7a9d5
Louka <louka.ortega-cand@epitech.eu>
2025-10-15 18:03:55 +0200

    fix: fix collision

--- gameEngine/systems/collision/src/Collision.hpp
+++ gameEngine/systems/collision/src/Collision.hpp
@@ -17,9 +17,12 @@ class Collision : public System<Collision>
 {
    private:
     void collide(uint32_t e1, uint32_t e2, Registry& registry) {
+        if (e1 == e2)
+            return;
 
         GameEngine::Position e1Pos = registry.get<GameEngine::Position>(e1);
         GameEngine::Position e2Pos = registry.get<GameEngine::Position>(e2);
+
         GameEngine::Collider e1Collider = registry.get<GameEngine::Collider>(e1);
         GameEngine::Collider e2Collider = registry.get<GameEngine::Collider>(e2);
 
@@ -32,7 +35,11 @@ class Collision : public System<Collision>
         vec2 e1HitboxPos = e1Pos.pos + e1Collider.originTranslation;
         vec2 e2HitboxPos = e2Pos.pos + e2Collider.originTranslation;
 
-        if ((e1Collider.entitySelector & e2Collider.entitySelector).any() && e1HitboxPos.x < e2HitboxPos.x + e2Collider.size.x && e1HitboxPos.x + e1Collider.size.x > e2HitboxPos.x && e1HitboxPos.y < e2HitboxPos.y + e2Collider.size.y && e1HitboxPos.y + e1Collider.size.y > e2HitboxPos.y) {
+        if ((e1Collider.entitySelector & e2Collider.entitySelector).any() &&
+            e1HitboxPos.x < e2HitboxPos.x + e2Collider.size.x &&
+            e1HitboxPos.x + e1Collider.size.x > e2HitboxPos.x &&
+            e1HitboxPos.y < e2HitboxPos.y + e2Collider.size.y &&
+            e1HitboxPos.y + e1Collider.size.y > e2HitboxPos.y) {
             e1Health.currentHp = e1Health.currentHp > 0 ? std::max(e1Health.currentHp - e2Damage.dmg, 0) : e1Health.currentHp;
             e2Health.currentHp = e2Health.currentHp > 0 ? std::max(e2Health.currentHp - e1Damage.dmg, 0) : e2Health.currentHp;
         }
@@ -49,19 +56,19 @@ class Collision : public System<Collision>
     {
         updateCount++;
 
-        GameEngine::Renderable screenInfo =
-            registry.get<GameEngine::Renderable>(0);
-        uint32_t screenSizeX = screenInfo.screenSizeX;
-        uint32_t screenSizeY = screenInfo.screenSizeY;
+        uint32_t screenSizeX = 1920;
+        uint32_t screenSizeY = 1080;
         uint32_t hitboxSizeMean = 64;
 
-        size_t gridWidth = screenSizeX / hitboxSizeMean;
-        size_t gridHeight = screenSizeY / hitboxSizeMean;
+        int gridWidth = screenSizeX / hitboxSizeMean + 1;
+        int gridHeight = screenSizeY / hitboxSizeMean + 1;
 
         std::vector<std::vector<std::vector<uint32_t>>> grid(
             gridWidth, std::vector<std::vector<uint32_t>>(gridHeight));
 
         registry.each<Position, Renderable, Collider, Damage, Health>([dt, &grid, hitboxSizeMean](auto e, Position& pos, Renderable& render, Collider& collider, Damage &damage, Health& health) {
+            if (pos.pos.x < 0 || pos.pos.y < 0)
+                return;
             float rightPos = pos.pos.x + collider.size.x;
             float topPos = pos.pos.y + collider.size.y;
 
@@ -70,36 +77,36 @@ class Collision : public System<Collision>
             int rightCell = rightPos / hitboxSizeMean;
             int topCell = topPos / hitboxSizeMean;
 
-            for (size_t i = leftCell; i <= rightCell; i++) {
-                for (size_t j = bottomCell; i <= topCell; i++) {
+            for (int i = leftCell; i <= rightCell; i++) {
+                for (int j = bottomCell; j <= topCell; j++) {
                     grid[i][j].push_back(e);
                 }
             }
         });
-        for (size_t i = 0; i < grid.size(); i++) {
-            for (size_t j = 0; j < grid[i].size(); j++) {
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
                 while (grid[i][j].size() > 1) {
-                    for (size_t k = 1; k < grid[i][j].size(); k++) {
+                    for (int k = 1; k < grid[i][j].size(); k++) {
                         collide(grid[i][j][0], grid[i][j][k], registry);
                     }
                     if (i + 1 < grid.size()) {
-                        for (size_t k = 0; k < grid[i + 1][j].size(); k++) {
+                        for (int k = 0; k < grid[i + 1][j].size(); k++) {
                             collide(grid[i][j][0], grid[i + 1][j][k], registry);
                         }
                     }
                     if (j + 1 < grid[i].size()) {
-                        for (size_t k = 0; k < grid[i][j + 1].size(); k++) {
+                        for (int k = 0; k < grid[i][j + 1].size(); k++) {
                             collide(grid[i][j][0], grid[i][j + 1][k], registry);
                         }
                     }
                     if (i + 1 < grid.size() && j + 1 < grid[i].size()) {
-                        for (size_t k = 0; k < grid[i + 1][j + 1].size();
+                        for (int k = 0; k < grid[i + 1][j + 1].size();
                              k++) {
                             collide(grid[i][j][0], grid[i + 1][j + 1][k], registry);
                         }
                     }
-                    if (i - 1 >= 0 && j + 1 < grid[i].size()) {
-                        for (size_t k = 0; k < grid[i - 1][j + 1].size(); k++) {
+                    if (i - 1 >= 0 && j + 1 < grid[i - 1].size()) {
+                        for (int k = 0; k < grid[i - 1][j + 1].size(); k++) {
                             collide(grid[i][j][0], grid[i - 1][j + 1][k], registry);
                         }
                     }
