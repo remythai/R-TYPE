b7145c0846cb0e84f17047ff59a44865be3924bc
Antton Ducos <antton.ducos@epitech.eu>
2025-10-27 10:02:02 +0100

    feat: add components that making sharedlib for motion test

--- gameEngine/components/position/src/Position.hpp
+++ gameEngine/components/position/src/Position.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "../../../ecs/utils.hpp"
+#include "../../../ecs/Component.hpp"
 
 namespace GameEngine {
-struct Position
-{
+struct Position : public Component<Position> {
     vec2 pos;
     Position(float val_x = 0, float val_y = 0) : pos(val_x, val_y) {}
+
+    static constexpr const char* Name = "Position";
+    static constexpr const char* Version = "1.0.0";
 };
 }  // namespace GameEngine
