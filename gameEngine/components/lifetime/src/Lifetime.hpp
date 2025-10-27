86528d4c968c0e06d8044ce4a4514c3752101449
Antton Ducos <antton.ducos@epitech.eu>
2025-10-27 15:08:26 +0100

    feat: add Lifetime in sharedlib

--- gameEngine/components/lifetime/src/Lifetime.hpp
+++ gameEngine/components/lifetime/src/Lifetime.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "../../../ecs/Component.hpp"
+
 namespace GameEngine {
-struct Lifetime
+struct Lifetime : public Component<Lifetime>
 {
     float time;
     Lifetime(float val_time = 0) : time(val_time) {}
+
+    static constexpr const char* Name = "Lifetime";
+    static constexpr const char* Version = "1.0.0";
 };
 }  // namespace GameEngine
