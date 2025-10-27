231374d793552fffe2fb0d9ac521c091006d8034
remythai <remy.thai@epitech.eu>
2025-10-27 20:22:23 +0100

    feat: colorBlindFilter

---
+++ client/src/colorBlindFilter/ColorBlindFilter.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2025
+** RTypeClient
+** File description:
+** ColorBlindFilter.hpp
+*/
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace CLIENT {
+
+enum class ColorBlindMode {
+    NONE,
+    PROTANOPIA,
+    DEUTERANOPIA,
+    TRITANOPIA
+};
+
+class ColorBlindFilter {
+public:
+    ColorBlindFilter();
+    ~ColorBlindFilter() = default;
+
+    void setMode(ColorBlindMode mode);
+    ColorBlindMode getMode() const { return _currentMode; }
+    
+    const sf::RenderStates* getRenderStates() const;
+    bool isActive() const { return _currentMode != ColorBlindMode::NONE && _shaderLoaded; }
+    
+    static std::string getModeName(ColorBlindMode mode);
+
+private:
+    void updateShader();
+
+    ColorBlindMode _currentMode;
+    sf::Shader _shader;
+    bool _shaderLoaded;
+};
+
+} // namespace CLIENT
