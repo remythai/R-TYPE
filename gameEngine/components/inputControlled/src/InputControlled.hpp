5816ee895fc036a9ac769fb52cf52c98b6d68803
Antton Ducos <antton.ducos@epitech.eu>
2025-10-27 15:05:23 +0100

    feat: add InputControlled in sharedlib

--- gameEngine/components/inputControlled/src/InputControlled.hpp
+++ gameEngine/components/inputControlled/src/InputControlled.hpp
@@ -2,11 +2,17 @@
 
 #include <string>
 #include <vector>
+
+#include "../../../ecs/Component.hpp"
+
 namespace GameEngine {
-struct InputControlled
+struct InputControlled : public Component<InputControlled>
 {
     std::vector<int> inputs;
     bool firstInput;
     InputControlled() : firstInput(false) {}
+
+    static constexpr const char* Name = "InputControlled";
+    static constexpr const char* Version = "1.0.0";
 };
 }  // namespace GameEngine
