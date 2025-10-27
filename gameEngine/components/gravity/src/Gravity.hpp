e38401d49fbf33083cbd6f44a17759c341bf3be0
Antton Ducos <antton.ducos@epitech.eu>
2025-10-27 15:02:16 +0100

    feat: add Gravity in sharedlib

--- gameEngine/components/gravity/src/Gravity.hpp
+++ gameEngine/components/gravity/src/Gravity.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "../../../ecs/Component.hpp"
+
 namespace GameEngine {
-struct Gravity
+struct Gravity : public Component<Gravity>
 {
     float force;
-    Gravity(float val_force) : force(val_force) {}
+    Gravity(float val_force = 0) : force(val_force) {}
+
+    static constexpr const char* Name = "Gravity";
+    static constexpr const char* Version = "1.0.0";
 };
 }  // namespace GameEngine
