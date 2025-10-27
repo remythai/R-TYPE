bee2543e65c13e63fde06df9c5937850bb6e75cd
Antton Ducos <antton.ducos@epitech.eu>
2025-10-27 14:58:50 +0100

    feat: AIControlled, Audio, Collider and Damage in sharedlib

--- gameEngine/components/damage/src/Damage.hpp
+++ gameEngine/components/damage/src/Damage.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "../../../ecs/Component.hpp"
+
 namespace GameEngine {
-struct Damage
+struct Damage : public Component<Damage>
 {
     int dmg;
-    Damage(int val_dmg) : dmg(val_dmg) {}
+    Damage(int val_dmg = 0) : dmg(val_dmg) {}
+
+    static constexpr const char* Name = "Damage";
+    static constexpr const char* Version = "1.0.0";
 };
 }  // namespace GameEngine
