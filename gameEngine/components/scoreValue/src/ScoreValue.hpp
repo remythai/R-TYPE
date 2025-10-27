7e2632134738a1032fbebb68a9a98ea498ce9786
Antton Ducos <antton.ducos@epitech.eu>
2025-10-27 15:14:13 +0100

    feat: add ScoreValue in sharedlib

--- gameEngine/components/scoreValue/src/ScoreValue.hpp
+++ gameEngine/components/scoreValue/src/ScoreValue.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "../../../ecs/Component.hpp"
+
 namespace GameEngine {
-struct ScoreValue
+struct ScoreValue : public Component<ScoreValue>
 {
     int points;
     ScoreValue(int val_points = 0) : points(val_points) {}
+
+    static constexpr const char* Name = "ScoreValue";
+    static constexpr const char* Version = "1.0.0";
 };
 }  // namespace GameEngine
